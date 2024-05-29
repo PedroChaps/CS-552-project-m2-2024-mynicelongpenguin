859c7c4c9df42c48fbc9968a186e53aacd81e76b
Pierre Hogenhaug <hogenhau@izar1.izar.cluster>
2024-05-29 21:01:26 +0200

    Updated tensorboard script

--- model/models/tensorboard.sh
+++ model/models/tensorboard.sh
@@ -5,6 +5,5 @@ module load gcc python openmpi py-tensorflow
 
 ipnport=$(shuf -i8000-9999 -n1)
 
-#TENSOR_DIR=$1
-TENSOR_DIR=/scratch/izar/aloureir/project-m2-2024-mynicelongpenguin/model/models/outputs/combined_40k_model/runs/May24_22-49-14_i28
+TENSOR_DIR=$1
 tensorboard --logdir ${TENSOR_DIR}  --port=${ipnport} --bind_all
