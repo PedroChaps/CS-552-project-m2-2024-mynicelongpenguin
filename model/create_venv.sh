5fc798a5fc5f82107b0e47a539574e3a4ee9fc3c
Pierre Hogenhaug <hogenhau@i48.izar.cluster>
2024-05-29 21:40:03 +0200

    Tested creating venv, did some changes and run evalutor script

--- model/create_venv.sh
+++ model/create_venv.sh
@@ -1,5 +1,7 @@
 # /bin/bash
 
+module load gcc python cuda
+
 virtualenv --system-site-packages ~/venvs/venv_evaluator
 
 source ~/venvs/venv_evaluator/bin/activate
@@ -8,7 +10,7 @@ source ~/venvs/venv_evaluator/bin/activate
 pip install --upgrade pip
 
 pip3 install torch==2.1.1 --index-url https://download.pytorch.org/whl/cu118
-pip3 install -r ./train_dpo/requirements.txt
+pip3 install -r ./requirements.txt
 pip3 install -U git+https://github.com/huggingface/trl
 
 deactivate
