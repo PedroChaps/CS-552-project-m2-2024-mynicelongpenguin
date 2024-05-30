9b9fa8af2e67e2fa1b46378fdbc77cf7a61d8497
André Espírito Santo <andre.loureiroespiritosanto@epfl.ch>
2024-05-30 14:26:54 +0200

    Make it sbatch able

--- model/run_evaluator.sh
+++ model/run_evaluator.sh
@@ -1,4 +1,13 @@
 #! /bin/bash -l
+#SBATCH --chdir /scratch/izar/aloureir/project-m2-2024-mynicelongpenguin/model
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 1
+#SBATCH --mem 80G
+#SBATCH --time 05:00:00
+#SBATCH --gres gpu:1
+#SBATCH --account cs-552
+#SBATCH --qos cs-552
+
 module load gcc python cuda
 
 echo "Loading virtualenv..."
