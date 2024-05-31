79957430ef561d2d4ba79b7c7e36701382eee925
André Espírito Santo <andre.loureiroespiritosanto@epfl.ch>
2024-05-31 02:39:40 +0200

    Updated M3 deliverable

--- model/models/train_sft_mcq/sft.sh
+++ model/models/train_sft_mcq/sft.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 1
 #SBATCH --mem 80G
-#SBATCH --time 71:59:59
+#SBATCH --time 20:00:00
 #SBATCH --gres gpu:1
 #SBATCH --account cs-552
 #SBATCH --qos cs-552
@@ -18,22 +18,22 @@ SAMPLE=3000
 
 # You only need to create this virtualenv once
 # Feel free to replace the name "course_py-3.10" with your own environemnt name
-# virtualenv --system-site-packages ~/venvs/mnlp2
+# virtualenv --system-site-packages ~/venvs/sft_mcq
 
 # Activate the virtualenv everytime before you run the job
 # Make sure the name matches the one you created
-source ~/venvs/mnlp2/bin/activate
+source ~/venvs/sft_mcq/bin/activate
 
 # huggingface-cli login --token $HF_TOKEN --add-to-git-credential
-huggingface-cli login --token $HF_TOKEN 
+# huggingface-cli login --token $HF_TOKEN 
 
 # upgrade pip the first time you load the environment
 # pip install --upgrade pip
 
 
 # Only when you need to update any packages you already installed
-# pip3 install torch==2.1.1 --index-url https://download.pytorch.org/whl/cu118
-# pip3 install -r ./train_dpo/requirements.txt
+# pip3 install torch==2.1.2 --index-url https://download.pytorch.org/whl/cu118
+# pip3 install -r ./train_sft_mcq/requirements.txt
 # pip3 install -U git+https://github.com/huggingface/trl
 
 
@@ -41,12 +41,14 @@ echo "Install Dependency completed"
 
 echo "Going to run the program..."
 
-python3 ./train_dpo/dpo/train_dpo.py --lr 1e-5 \
-    --beta 0.3 \
-    --label_smoothing 0.1 \
-    --dataset_name "/scratch/izar/aloureir/project-m2-2024-mynicelongpenguin/data/combined_40k_train.jsonl" \
-    --output_dir "/scratch/izar/aloureir/project-m2-2024-mynicelongpenguin/model/models/outputs/final_model_40k" \
-    --effective_batch_size 32
+
+    # --dataset_name "/scratch/izar/aloureir/project-m2-2024-mynicelongpenguin/data/Transformed_DeepMind_Algebra_QA_with_explanation.jsonl" \
+    # --sample_quantity 10000 \
+python3 train_sft_mcq/sft/train_sft.py --lr 1e-5 \
+    --dataset_name "/scratch/izar/aloureir/project-m2-2024-mynicelongpenguin/data/Transformed_SciQ_Dataset_with_explanation.jsonl" \
+    --output_dir "/scratch/izar/aloureir/project-m2-2024-mynicelongpenguin/model/models/outputs/sft_model_try" \
+    --effective_batch_size 32 \
+    --adapter_model_name "pleaky2410/TeachersHateChatbots" \
 
 
 echo "Test complete on $(hostname)"
--- model/models/train_sft_mcq/sft/train_sft.py
+++ model/models/train_sft_mcq/sft/train_sft.py
@@ -14,7 +14,7 @@ from transformers import (
     BitsAndBytesConfig,
 )
 
-from peft import LoraConfig
+from peft import LoraConfig, PeftModel
 import argparse
 
 
@@ -27,7 +27,7 @@ def dprint(*args, **kwargs):
 # SOME PARAMETERS (may adjust in the code as well)
 # username = "aloureir"
 
-# model_name = "rhysjones/phi-2-orange-v2"
+# base_model_name = "pleaky2410/TeachersHateChatbots"
 
 # dataset_name = f"/scratch/izar/{username}/project-m2-2024-mynicelongpenguin/data/combined_40k_train.jsonl"
 
@@ -35,17 +35,21 @@ def dprint(*args, **kwargs):
 # resume_from_checkpoint = None #set to the path of the checkpoint to resume training from
 
 # output_dir = "outputs/chosen_model"
-push_to_hub = True #set True to push the model to the hub at the end of training
+push_to_hub = False #set True to push the model to the hub at the end of training
 commit_message = "First model, trained on 40k examples for 3 epochs" #commit message
-repo_id = "TeachersHateChatbots"
+repo_id = "TeachersHateChatbotsSFT"
 ##################################################################
-ANSWER_TEMPLATE = """
-### EXPLANATION
+ANSWER_TEMPLATE = """### EXPLANATION
 <explanation>
 
 ### ANSWER
-<correct_option>
-"""
+<correct_option>"""
+
+QUESTION_TEMPLATE = """### QUESTION
+<question>
+
+###OPTIONS
+<options>"""
 
 
 def get_mcq_options(samples):
@@ -58,8 +62,28 @@ def get_mcq_options(samples):
         "explanation": [str]
     }
     """
-    raise NotImplementedError
+    questions_and_options = samples["question"]
+    remove_prefix = len("Question: ")
+    remove_suffix = len("\n\nAnswer")
 
+    questions_and_options = [question_and_options[remove_prefix:-remove_suffix] for question_and_options in questions_and_options]
+
+    questions_and_options = [question_and_options.split("\n\nOptions:\n") for question_and_options in questions_and_options]
+
+    questions, options = zip(*questions_and_options)
+    questions = list(questions)
+
+    options = list(options)
+    options = [opts.split("\n") for opts in options]
+    options = [[opt for opt in opts if opt != "" and opt != " "] for opts in options]
+
+    to_remove = ["A)", "B)", "C)", "D)", "E)", "F)", "A. ", "B. ", "C. ", "D. ", "E. ", "F. "]
+
+    for remove in to_remove:
+        options = [[opt.replace(remove, "") if opt.startswith(remove) else opt for opt in opts] for opts in options]
+
+    return {"question": questions, "options": options, "explanation": samples["explanation"], "correct_option": samples["answer"]}
+    
 
 def create_datasets(args, tokenizer):
     def apply_template(samples):
@@ -68,14 +92,24 @@ def create_datasets(args, tokenizer):
         messages = []
 
         for i, question in enumerate(samples["question"]):
+            prompt = QUESTION_TEMPLATE.replace("<question>", question)
+            options=""
+            for j, opt in enumerate(samples["options"][i]):
+                options += f"{chr(ord('A') + j)}: {opt}\n"
+
+            prompt = prompt.replace("<options>", options)
+
             conversation = [system]
-            conversation.append({"role": "user", "content": question})
+            conversation.append({"role": "user", "content": prompt})
             
             answer =  ANSWER_TEMPLATE.replace("<explanation>", samples["explanation"][i]).replace("<correct_option>", samples["correct_option"][i])
-            
+
             conversation.append({"role": "assistant", "content": answer})
 
-            # texts.append(tokenizer.apply_chat_template(conversation, tokenize=False, add_generation_prompt=False))
+            # dprint(conversation)
+
+            # dprint("### CONVERSATION")
+            # dprint(tokenizer.apply_chat_template(conversation, tokenize=False, add_generation_prompt=False))
 
             messages.append(conversation)
 
@@ -84,9 +118,10 @@ def create_datasets(args, tokenizer):
 
     dataset = load_dataset("json", data_files=args.dataset_name, split="train")
 
-
+    dataset = dataset.shuffle(seed=args.seed)
     if args.sample_quantity:
-        dataset = dataset.shuffle(seed=args.seed).select(range(args.sample_quantity))
+        dataset = dataset.select(range(args.sample_quantity))
+
 
     dataset = dataset.train_test_split(test_size=0.1)
 
@@ -108,37 +143,48 @@ def load_model(args):
     # 2. Load the model and adapter, merge them and unload adapter, and train new adapter on top of the already finetuned one
     # 3. Load the model and do the adapter thing they suggest for DPO (see DPOTRainer HuggingFace page, Option 3)
 
-        model = AutoModelForCausalLM.from_pretrained(
-            args.model_name,
-            # quantization_config=bnb_config,
-            trust_remote_code=True,
-            # torch_dtype=torch.fp32,
-            torch_dtype=torch.float32,
-            device_map="auto",
-        )
+    model = AutoModelForCausalLM.from_pretrained(
+        args.base_model_name,
+        # quantization_config=bnb_config,
+        trust_remote_code=True,
+        # torch_dtype=torch.fp32,
+        torch_dtype=torch.float32,
+        device_map="auto",
+    )
+
+    print(model)
+    print(model.num_parameters())
+
+    if args.adapter_model_name:
+        print("MODEL IS PEFT MODEL")
+
+        #Load the model and merge it so we can stack another adapter on top
+        model = PeftModel.from_pretrained(model, args.adapter_model_name)
+        print(model)
+        model = model.merge_and_unload(safe_merge=True, progressbar=True)
+
+        print(model)
+        print(model.num_parameters())
+
+    tokenizer = AutoTokenizer.from_pretrained(args.adapter_model_name if args.adapter_model_name else args.base_model_name)
+    
+    dprint(f"EOS token: {tokenizer.eos_token}\tID: {tokenizer.eos_token_id}")
+    dprint(f"BOS token: {tokenizer.bos_token}\tID: {tokenizer.bos_token_id}")
+    dprint(f"PAD token: {tokenizer.pad_token}\tID: {tokenizer.pad_token_id}")
+    dprint(f"CHAT TEMPLATE: {tokenizer.chat_template}")
+    dprint()
 
 
-        # special_tokens = ChatmlSpecialTokens
-        # chat_template = DEFAULT_CHATML_CHAT_TEMPLATE
+    peft_config = LoraConfig(
+        lora_alpha=16,
+        lora_dropout=0.1,
+        r=16,
+        bias="none",
+        task_type="CAUSAL_LM",
+        target_modules="all-linear"
+    )
 
-        tokenizer = AutoTokenizer.from_pretrained(args.model_name)
-        
-        dprint(tokenizer.pad_token)
-        dprint(tokenizer.eos_token)
-        dprint(tokenizer.bos_token)
-        dprint(tokenizer.chat_template)
-
-
-        peft_config = LoraConfig(
-            lora_alpha=16,
-            lora_dropout=0.1,
-            r=16,
-            bias="none",
-            task_type="CAUSAL_LM",
-            target_modules="all-linear"
-        )
-
-        return model, tokenizer, peft_config
+    return model, tokenizer, peft_config
     
 
 
@@ -160,22 +206,21 @@ def main(args):
     training_args = TrainingArguments(
         output_dir=args.output_dir,
         evaluation_strategy="steps",
-        eval_steps=500,
+        eval_steps=100,
         per_device_train_batch_size=2,
-        per_device_eval_batch_size=2,
+        per_device_eval_batch_size=4,
         gradient_accumulation_steps=args.effective_batch_size // 2,
         gradient_checkpointing=True,
         learning_rate=args.lr,
         # weight_decay=5e-5,
         # warmup_ratio=0.0,
         lr_scheduler_type="cosine",
-        max_length=2048,
         num_train_epochs=3,
         logging_steps=5,
         log_level="info",
         logging_strategy="steps",
         save_strategy="steps",
-        save_steps=500,
+        save_steps=200,
         optim="paged_adamw_32bit",
         warmup_steps=5,
         max_grad_norm=1.0,
@@ -209,6 +254,7 @@ def main(args):
         tokenizer=tokenizer,
         # dataset_text_field="text",
         peft_config=peft_config,
+        max_seq_length=2048,
         data_collator=collator
     )
 
@@ -229,7 +275,8 @@ if __name__ == "__main__":
     parser.add_argument("--dataset_name", type=str, help="Dataset for training")
     parser.add_argument("--sample_quantity", type=int, default=None, help="Number of entries to sample")
     parser.add_argument("--seed", type=int, default=42, help="Seed")
-    parser.add_argument("--model_name", type=str, default="rhysjones/phi-2-orange-v2", help="Base model")
+    parser.add_argument("--adapter_model_name", type=str, default=None, help="Base model")
+    parser.add_argument("--base_model_name", type=str, default="rhysjones/phi-2-orange-v2", help="Base model")
     parser.add_argument("--resume_from_checkpoint", type=str, default=None, help="Checkpoint to resume training from")
     parser.add_argument("--output_dir", type=str, help="Directory to output the model")
     parser.add_argument("--effective_batch_size", type=int, default=32, help="Effective batch size")
